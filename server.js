6cf4f5878f4554f31bd9dd65046211c70832648e
louis mane <dennis.itua1@gmail.com>
2021-07-30 22:58:50 -0400

    Update server with middleware

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 mongoose.connect("mongodb://localhost/autoGurus_db", 
 {
     useNewUrlParser: true,
@@ -16,9 +19,6 @@ mongoose.connection.on("error", (err) => {
     console.log("error connecting", err)
 })
 mongoose.set('useFindAndModify', false);
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use(require('./routes/posts'));
 app.use(require('./routes/users'));
