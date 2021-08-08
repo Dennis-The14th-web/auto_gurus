9031c3bb703d7cc52682c6412be0448cf8179a9e
louis mane <dennis.itua1@gmail.com>
2021-08-08 14:02:11 -0400

    Updated reducers with auth.js from actionConstants

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,6 +1,19 @@
-// import all constants instances with the alias
-// import * as actionConstants from '../constants/actionConstants';
+// import all/(*) constants instances with the alias
+import * as actionConstants from '../constants/actionConstants';
 
-// const authReducer = (state = { authData: null }, action) => {
-//     switch (action.)
-// }
+const authReducer = (state = { authData: null }, action) => {
+    switch (action.type) {
+        case actionConstants.AUTH_INSTANCE:
+            localStorage.setItem('profile', JSON.stringify({ ...action?.data }));
+
+            return { ...state, authData: action.data, loading: false, errors: null };
+        case actionConstants.LOGOUT_INSTANCE:
+            localStorage.clear();
+
+            return { ...state, authData: null, loading: false, errors: null };
+        default:
+            return state;
+    }
+};
+
+export default authReducer;
