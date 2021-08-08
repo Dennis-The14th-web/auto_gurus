d902c229e02b8141ef08e70b9953f27a318ad755
louis mane <dennis.itua1@gmail.com>
2021-08-08 12:30:43 -0400

    Updated actionConstants

---
+++ client/src/constants/actionConstants.js
@@ -0,0 +1,13 @@
+export const CREATE_INSTANCE          = 'CREATE_INSTANCE';
+export const UPDATE_INSTANCE          = 'UPDATE_INSTANCE';
+export const DELETE_INSTANCE          = 'DELETE_INSTANCE';
+export const FETCH_ALL_INSTANCE       = 'FETCH_ALL_INSTANCE';
+export const FETCH_BY_SEARCH_INSTANCE = 'FETCH_BY_SEARCH_INSTANCE';
+export const FETCH_POST_INSTANCE      = 'FETCH_POST_INSTANCE';
+export const START_LOADING_INSTANCE   = 'START_LOADING_INSTANCE';
+export const END_LOADING_INSTANCE     = 'END_LOADING_INSTANCE';
+export const LIKE_INSTANCE            = 'LIKE_INSTANCE';
+export const AUTH_INSTANCE            = 'AUTH_INSTANCE';
+export const LOGIN_INSTANCE           = 'LOGIN_INSTANCE';
+export const LOGOUT_INSTANCE          = 'LOGOUT_INSTANCE';
+
