0ade62266dc75007350def5d3d32e64bdb344cf3
louis mane <dennis.itua1@gmail.com>
2021-08-08 13:23:03 -0400

    Update api module

---
+++ client/src/api/index.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API = '/posts';
+
+axios.interceptors.request.use(req => {
+    if(localStorage.getItem('profile')) {
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+    return req;
+})
+
+export const fetchPost        = id => axios.get(`${API}/${id}`);
+export const fetchPosts       = page => axios.get(`${API}?page=${page}`);
+export const fetchPostsBySrch = srchQuery => axios.get(`${API}/search?searchQuery=${srchQuery}`);
+export const createPost       = newPost => axios.post(`${API}`, newPost);
+export const likePost         = id => axios.patch(`${API}/${id}/likePost`);
+export const updatePost       = (id, updatedPost) => axios.patch(`${API}/${id}`, updatedPost);
+export const deletePost       = id => axios.delete(`${API}/${id}`);
+
+// Authentication call
+export const signIn           = reqBody => axios.post('/user/signin', reqBody);
+export const signUp           = reqBody => axios.post('/user/signup', reqBody);
